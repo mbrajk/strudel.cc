a7e2827e9c874e8a72bb3dbadb5c5060ac1ecc48
mbrajk <mbrajk@users.noreply.github.com>
2025-08-06 23:31:04 -0500

    add track: demoing things

---
+++ demoing_things.js
@@ -0,0 +1,4 @@
+$: note("<[48 67 63 [62, 58]]!2>, <[55 - - 50]!2 [53 - - 50]!2 [44 - - 50]!2 [43 - - 50]!2>")
+  .sound("piano gm_electric_guitar_muted").cpm(30).room(0.8)
+
+$: sound("bd [<rim hh>]").gain("[0.70 [<.50 1>]]").bank("RolandTR707 [<RolandTR707 RolandTR909>]").delay(".5")
